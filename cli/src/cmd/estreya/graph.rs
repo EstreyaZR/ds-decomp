8e53ca5b5c943e6e386b83736a52bc83a2c824a6
EstreyaZR <zrezlon@gmail.com>
2026-09-21 22:50:49 +0200

    Implemented Directory-based CLI, Tagging Pipeline, first drafts of Grouping

--- cli/src/cmd/estreya/graph.rs
+++ cli/src/cmd/estreya/graph.rs
@@ -2,13 +2,18 @@ use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::Args;
-use ds_decomp::{analysis::graph::Graph, config::config::Config};
+use ds_decomp::{
+    analysis::graph::{Graph, GraphNodes, GraphParser},
+    config::config::Config,
+};
+
+use crate::util::io::read_dir;
 #[derive(Args)]
 pub struct EstreyaComGraphArgs {
     #[arg(long, short = 'c')]
     pub config_path: PathBuf,
     #[arg(long, short = 'a')]
-    pub asm_file: PathBuf,
+    pub asm_dir: PathBuf,
     #[arg(long, short = 'd')]
     pub dry_run: bool,
 }
@@ -17,9 +22,30 @@ impl EstreyaComGraphArgs {
     pub fn run(&self) -> Result<()> {
         let _config = Config::from_file(&self.config_path)?;
         let _config_path = self.config_path.parent().unwrap();
-        let asm_file = &self.asm_file;
-
-        Graph::from_file(asm_file);
+        if let Ok(nodes) = Self::dir_crawler(&self.asm_dir) {
+            Graph::with_graph_nodes(nodes);
+        }
         Ok(())
+    }
+
+    pub fn dir_crawler(path: &PathBuf) -> Result<GraphNodes> {
+        let mut vec = GraphNodes::default();
+
+        let dir = read_dir(path).unwrap();
+        for file in dir {
+            let file = file.unwrap();
+            let path = file.path();
+            if path.is_dir() {
+                if let Ok(nodes) = Self::dir_crawler(&path) {
+                    vec.0.extend(nodes);
+                }
+            } else {
+                let entries = GraphParser::parse(path).unwrap();
+                let nodes = entries.to_nodes();
+                let nodes = nodes.resolve_labels();
+                vec.0.extend(nodes.0);
+            }
+        }
+        Ok(vec)
     }
 }
--- lib/src/analysis/graph.rs
+++ lib/src/analysis/graph.rs
@@ -1,27 +1,57 @@
 use std::{
-    fmt::{Display, LowerHex, write},
+    collections::BTreeMap,
+    fmt::{Display, LowerHex},
     io::Write,
     path::Path,
+    str::FromStr,
     vec::IntoIter,
 };
 
 use snafu::Whatever;
+use strum::EnumString;
 
 use crate::util::{io::read_to_string, parse::parse_u16};
 #[derive(Default, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
 pub struct Graph {}
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+#[allow(unused)]
+#[derive(EnumString, Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
 enum GraphFileType {
-    NCLR,
-    NSCR,
+    Nclr,
+    Nscr,
+    Bmg,
+    Ncer,
+    Bnll,
+    Bnbl,
+    Ncgr,
+}
+#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+enum GraphNodeStatus {
+    /// Unprocessed
+    #[default]
+    Init,
+    /// Only called by others
+    Leaf,
+    /// Has Children, no known Level.
+    NodeUnkLevel,
+    /// Alternative to [GraphNodeStatus::NodeUnkLevel], to stop LevelAnalysis
+    Node,
+    /// Unused Symbol?
+    Orphan,
+    /// TODO: Level of distance to a leaf, where a node directly next to a leaf child is 1.
+    NodeWithLevel(u32),
+    /// Has gone through Tagging,
+    Tagged,
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+#[allow(unused)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 enum GraphNodeTag {
     Address,
     File(GraphFileType),
     Collection,
+    // Symbol only found after reading all references
+    GeneratedByGraph,
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
@@ -55,11 +85,26 @@ impl GraphNodeWord {
     }
 }
 
-#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Default)]
 struct GraphNodeTags(Vec<GraphNodeTag>);
 
+impl GraphNodeTags {
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+impl IntoIterator for GraphNodeTags {
+    type IntoIter = IntoIter<Self::Item>;
+    type Item = GraphNodeTag;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
 #[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone)]
-struct GraphNodes(Vec<GraphNode>);
+pub struct GraphNodes(pub Vec<GraphNode>);
 
 impl IntoIterator for GraphNodes {
     type IntoIter = IntoIter<Self::Item>;
@@ -75,34 +120,63 @@ impl GraphNodes {
         self.0.push(item);
     }
 
-    fn resolve_labels(&self) -> Self {
+    pub fn resolve_labels(&self) -> Self {
         let mut entries = Self::default();
         let iterator = self.clone().into_iter();
 
         let mut node = GraphNode { name: "START".into(), ..Default::default() };
-
+        let mut node_switch: bool = false; // turns true if Start has been skipped, workaround for the compiler since it wont allow NULL-ptr before the for-loop.
         for entry in iterator {
             node = match !entry.name.contains(".L_") {
                 true => {
+                    if !node_switch {
+                        node_switch = true;
+                        continue;
+                    }
                     node.clean();
                     entries.push(node);
                     entry
                 }
-                false => node.consume_label(&entry),
+                false => node.consume_label_node(&entry),
             }
         }
-
+        node.clean();
+        entries.push(node);
         entries
+    }
+
+    #[deprecated]
+    fn apply_tags(self) -> Self {
+        let iterator = self.into_iter();
+
+        let mut nodes = Self::default();
+
+        for mut node in iterator {
+            if node.is_collection() {
+                node.add_tag(GraphNodeTag::Collection);
+            } else if node.is_address() {
+                node.add_tag(GraphNodeTag::Address);
+            } else if let Some(file_type) = node.is_file() {
+                node.add_tag(GraphNodeTag::File(file_type));
+            }
+
+            node.update_status(GraphNodeStatus::Tagged);
+            nodes.push(node);
+        }
+
+        nodes
     }
 }
 
 #[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord, Clone)]
-struct GraphNode {
+pub struct GraphNode {
     name: String,
     tag: GraphNodeTags,
     byte: Vec<u16>,
     byte_as_str: Vec<String>,
     word: Vec<GraphNodeWord>,
+    called_by: Vec<GraphNodeWord>,
+    status: GraphNodeStatus,
 }
 
 impl GraphNode {
@@ -115,7 +189,7 @@ impl GraphNode {
     }
 
     fn consume_entry(self, entry: &GraphEntry) -> Self {
-        assert!(entry.name == None);
+        assert!(entry.name.is_none(), "Entry: {:#?} did not pass no-name check", entry);
         let name = self.name;
         let mut byte = self.byte;
         let mut byte_as_str = self.byte_as_str;
@@ -133,7 +207,7 @@ impl GraphNode {
         Self { name, byte, byte_as_str, word, ..Default::default() }
     }
 
-    fn consume_label(&self, label: &Self) -> Self {
+    fn consume_label_node(&self, label: &Self) -> Self {
         let mut word = self.word.clone();
         word.extend(label.word.clone());
 
@@ -165,28 +239,141 @@ impl GraphNode {
     }
 
     fn print(&self) {
-        let mut s = format!("[{}]\n", self.name);
+        let mut s = format!("[{}]\t{:?}\n", self.name, self.status);
+
+        if !self.tag.is_empty() {
+            s.push_str("[Tags]");
+            for i in self.tag.clone() {
+                s.push_str(&format!("\t{:?}\n", i));
+            }
+        }
 
         if !self.byte.is_empty() {
+            s.push_str(&format!("[Data]\tSize:{:#x}\n", self.byte_size()));
             s.push_str(&format!(
-                "Byte:\t{:x?}\nString:\t\"{}\"\n",
+                "\tByte:\t{:x?}\n\tString:\t\"{}\"\n",
                 self.byte,
                 self.byte_as_str.join("")
             ));
         }
         if !self.word.is_empty() {
-            s.push_str(&format!("Word:\n"));
+            s.push_str("[Word]\n");
             for i in &self.word {
+                s.push_str(&format!("\t{0:x}\n", i)); //
+            }
+        }
+        if !self.called_by.is_empty() {
+            s.push_str("[CalledBy]\n");
+            for i in &self.called_by {
                 s.push_str(&format!("\t{0:x}\n", i));
             }
         }
-        write!(&mut std::io::stdout(), "{}\n", s);
+        writeln!(&mut std::io::stdout(), "{}", s).unwrap();
+    }
+
+    /// Takes care of actually tagging [GraphNode],
+    /// please add calls for your tag check functions here,
+    /// and your tags to [GraphNodeTag]
+    fn apply_tags(&mut self) {
+        if self.is_collection() {
+            self.add_tag(GraphNodeTag::Collection);
+        } else if self.is_address() {
+            self.add_tag(GraphNodeTag::Address);
+        } else if let Some(file_type) = self.is_file() {
+            self.add_tag(GraphNodeTag::File(file_type));
+        }
+
+        self.update_status(GraphNodeStatus::Tagged);
+    }
+
+    /// Updates the [GraphNodeStatus] to the Level of Distance to a Leaf
+    fn apply_level(&mut self, nodes_btreemap: &BTreeMap<String, GraphNode>) {
+        if self.status == GraphNodeStatus::Tagged || self.status == GraphNodeStatus::NodeUnkLevel {
+            if self.word.is_empty() {
+                if self.called_by.is_empty() {
+                    self.update_status(GraphNodeStatus::Orphan);
+                } else {
+                    self.update_status(GraphNodeStatus::Leaf);
+                }
+            } else {
+                let mut level: u32 = 0;
+                for child_node in self.word.clone() {
+                    if let GraphNodeWord::Symbol(child_node) = child_node {
+                        if let Some(child_node) = nodes_btreemap.get(&child_node) {
+                            let child_level: u32 = match child_node.status {
+                                GraphNodeStatus::Leaf | GraphNodeStatus::Orphan => 1,
+                                GraphNodeStatus::NodeWithLevel(x) => x,
+                                GraphNodeStatus::Tagged
+                                | GraphNodeStatus::Init
+                                | GraphNodeStatus::NodeUnkLevel => 0,
+                                GraphNodeStatus::Node => u32::MAX,
+                            };
+                            level = u32::min(child_level, level);
+                        } else {
+                            continue;
+                        }
+                    }
+                }
+                match level {
+                    0 => self.update_status(GraphNodeStatus::NodeUnkLevel),
+                    1.. => self.update_status(GraphNodeStatus::NodeWithLevel(level)),
+                }
+            }
+        }
+    }
+
+    pub fn byte_size(&self) -> usize {
+        self.byte.len()
+    }
+
+    pub fn byte_are_padding(&self) -> bool {
+        for &i in &self.byte {
+            if i != 0 {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn is_collection(&self) -> bool {
+        !&self.word.is_empty()
+            && self.word.len() != 1
+            && (self.byte.is_empty() || self.byte_are_padding())
+    }
+
+    pub fn is_address(&self) -> bool {
+        !&self.word.is_empty() && self.byte.is_empty() && self.word.len() == 1
+    }
+
+    pub fn is_file(&self) -> Option<GraphFileType> {
+        let str = &self.byte_as_str.join("");
+        let s: Vec<_> = str.split(".").collect();
+        if let Some(last) = s.last() {
+            return GraphFileType::from_str(last).ok();
+        }
+        None
+    }
+
+    fn children(&self) -> Vec<GraphNodeWord> {
+        let mut calls = Vec::new();
+        for i in &self.word {
+            calls.push(i.to_owned())
+        }
+        calls
+    }
+
+    pub fn update_status(&mut self, status: GraphNodeStatus) {
+        self.status = status;
+    }
+
+    pub fn add_tag(&mut self, tag: GraphNodeTag) {
+        self.tag.0.push(tag);
     }
 }
 
 /// Struct that is built by every symbol / label declaration with form `"NAME: Option<WORD>"`
-#[derive(Default, PartialEq, Eq, PartialOrd, Ord, Clone)]
-struct GraphEntry {
+#[derive(Default, PartialEq, Eq, PartialOrd, Ord, Clone, Debug)]
+pub struct GraphEntry {
     name: Option<String>,
     label: bool,
     /// None: Symbol is a Label to jump to, used in goto, etc...
@@ -214,6 +401,7 @@ impl GraphEntry {
             let name = Some(source[0].strip_suffix(":").unwrap().to_string());
             if source.len() > 1 {
                 word = Some(source[2].to_string());
+                // println!("{:?}", &word);
             }
             return Ok(Self { name, label, word, ..Default::default() });
         }
@@ -296,18 +484,18 @@ impl Display for GraphEntry {
 
 /// Simple Vector Collection for [GraphEntry]
 #[derive(Default, PartialEq, Eq, PartialOrd, Ord, Clone)]
-struct GraphEntries(Vec<GraphEntry>);
+pub struct GraphEntries(Vec<GraphEntry>);
 
 impl GraphEntries {
     fn add(&mut self, x: GraphEntry) {
         self.0.push(x)
     }
 
-    fn collect_into_nodes(&self) -> GraphNodes {
+    pub fn to_nodes(&self) -> GraphNodes {
         let mut entries = GraphNodes::default();
         let iterator = self.clone().into_iter();
 
-        let mut node = GraphNode { name: "START".into(), ..Default::default() };
+        let mut node = GraphNode { name: "GRAPH_DUMMY_START".into(), ..Default::default() };
 
         for entry in iterator {
             node = match entry.name.is_some() {
@@ -319,6 +507,8 @@ impl GraphEntries {
                 false => node.consume_entry(&entry),
             }
         }
+
+        entries.push(node);
 
         entries
     }
@@ -333,10 +523,11 @@ impl IntoIterator for GraphEntries {
     }
 }
 
-struct GraphParser();
+pub struct GraphParser {}
 
+/// Parses a ASM File line-by-line to [GraphEntry], creating the collection [GraphEntries]
 impl GraphParser {
-    fn entries<P: AsRef<Path>>(file: P) -> Result<GraphEntries, Whatever> {
+    pub fn parse<P: AsRef<Path>>(file: P) -> Result<GraphEntries, Whatever> {
         let file = file.as_ref();
         let buffer = read_to_string(file).unwrap();
         let lines = buffer.lines();
@@ -356,20 +547,79 @@ impl GraphParser {
 }
 
 impl Graph {
-    #[allow(unused)]
-    fn new() -> Self {
-        Self { ..Default::default() }
-    }
-
-    pub fn from_file<P: AsRef<Path>>(file: P) /*-> Result<Self, Whatever>*/
+    pub fn with_graph_nodes(nodes: GraphNodes)
+    /*-> Result<Self, Whatever>*/
     {
-        let file = file.as_ref();
-        let entries = GraphParser::entries(file).unwrap();
-        let nodes = entries.collect_into_nodes();
-        let nodes = nodes.resolve_labels();
+        let mut nodes_btreemap: BTreeMap<String, GraphNode> = BTreeMap::new();
+        let mut caller_to_callee: BTreeMap<String, Vec<GraphNodeWord>> = BTreeMap::new();
+        let mut callee_to_caller: BTreeMap<GraphNodeWord, Vec<String>> = BTreeMap::new();
+
+        // for i in nodes.clone() {
+        //     i.print();
+        // }
+
+        for i in nodes.clone().into_iter() {
+            let children = i.children();
+            caller_to_callee.insert(i.name.clone(), children.clone());
+            for child in children {
+                if let Some(caller) = callee_to_caller.get(&child) {
+                    let mut caller = caller.clone();
+                    caller.push(i.name.clone());
+                    callee_to_caller.insert(child.to_owned(), caller);
+                } else {
+                    let caller = vec![i.name.clone()];
+                    callee_to_caller.insert(child.to_owned(), caller);
+                }
+            }
+        }
         for i in nodes.into_iter().as_ref() {
+            nodes_btreemap.insert(i.name.clone(), i.clone());
+        }
+        let mut unk_symbol_number = 1;
+        for (callee, caller) in callee_to_caller {
+            if let GraphNodeWord::Symbol(callee) = callee {
+                let caller = caller.iter().map(|x| GraphNodeWord::Symbol(x.to_string())).collect();
+
+                if let Some(node) = nodes_btreemap.get(&callee) {
+                    let mut node = node.clone();
+                    node.called_by = caller;
+                    nodes_btreemap.insert(node.name.clone(), node);
+                } else {
+                    let name = format!("UnknownSymbol_{unk_symbol_number}").to_string();
+                    let tags = GraphNodeTags(vec![GraphNodeTag::GeneratedByGraph]);
+                    nodes_btreemap.insert(name.clone(), GraphNode {
+                        name,
+                        tag: tags,
+                        status: GraphNodeStatus::Leaf,
+                        called_by: caller,
+                        ..Default::default()
+                    });
+                    unk_symbol_number += 1;
+                }
+            }
+        }
+
+        for node in nodes_btreemap.values_mut() {
+            node.apply_tags();
+        }
+
+        let mut counter: usize = 0;
+
+        loop {
+            let btreemap_last_iteration = nodes_btreemap.clone();
+            if counter < 50 {
+                for i in nodes_btreemap.values_mut() {
+                    i.apply_level(&btreemap_last_iteration);
+                }
+            } else {
+                break;
+            }
+            counter += 1;
+        }
+        for (_, i) in nodes_btreemap {
             i.print();
         }
+
         // for i in entries {
         //     println!("{}", i);
         // }
